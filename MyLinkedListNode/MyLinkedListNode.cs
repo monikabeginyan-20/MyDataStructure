ae2ab0b55f62e67cad63f44ff6fbd2048cf42677
User <User@DESKTOP-K6EQMDE>
2026-03-01 22:29:28 +0400

    Add project files.

---
+++ MyLinkedListNode/MyLinkedListNode.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LinkedList;
+
+public class MyLinkedListNode<T>
+{
+    public T Value { get; set; }
+    public MyLinkedListNode<T> Next { get; set; }
+
+    public MyLinkedListNode(T value)
+    {
+        Value = value;
+        Next = null;
+    }
+}
---
+++ TestingDataStructures/Program.cs
@@ -0,0 +1,26 @@
+﻿using LinkedList;
+
+namespace TestingDataStructures;
+
+public class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+        MyLinkedList<int> list = new MyLinkedList<int>();
+
+        list.Add(1);
+        list.Add(-1);
+        list.Add(2);
+        list.AddFirst(0);
+        list.AddLast(3);
+
+        Console.WriteLine(list + "\n");
+
+        foreach (var item in list)
+        {
+            Console.WriteLine(item);
+        }
+    }
+}
